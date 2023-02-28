8c6227652eae9e85aae109ad643160ab237d4668
Danielle Farias <Danielle_Fariass@hotmail.com>
2023-02-28 14:38:38 -0300

    exercicio22

--- src/main/java/com/capgeminischool/exercicio22/exercicio22.java
+++ src/main/java/com/capgeminischool/exercicio22/exercicio22.java
@@ -11,39 +11,39 @@ public class exercicio22 {
         float totalCusto = 0.0f;
         float totalVenda = 0.0f;
 
-        Scanner leitor = new Scanner(System.in);
+        try (Scanner leitor = new Scanner(System.in)) {
+            int i = 0; //escopo passou a ser td o metodo main, preciso desse numero para dividir pelo total de custo e total de venda no final.
+            for (; i < 3; i++){
 
-        int i = 0; //escopo passou a ser td o metodo main, preciso desse numero para dividir pelo total de custo e total de venda no final.
-        for (; i < 3; i++){
+                System.out.println("Digite o nome do produto: ");
+                nomeProduto = leitor.nextLine();
 
-            System.out.println("Digite o nome do produto: ");
-            nomeProduto = leitor.nextLine();
+                System.out.println("Digite o preço de custo do produto: ");
+                precoCusto = leitor.nextFloat();
 
-            System.out.println("Digite o preço de custo do produto: ");
-            precoCusto = leitor.nextFloat();
+                System.out.println("Digite o preço de venda do produto: ");
+                precoVenda = leitor.nextFloat();
 
-            System.out.println("Digite o preço de venda do produto: ");
-            precoVenda = leitor.nextFloat();
+                totalCusto = totalCusto + precoCusto;
+                totalVenda = totalVenda + precoVenda;
 
-            totalCusto = totalCusto + precoCusto;
-            totalVenda = totalVenda + precoVenda;
-
-            if(precoCusto == precoVenda){
-                System.out.println("Houve empate entre o preço de custo e o preço de venda");
-            }else{
-                if(precoCusto > precoVenda) {
-                    System.out.println("Prejuízo");
+                if(precoCusto == precoVenda){
+                    System.out.println("Houve empate entre o preço de custo e o preço de venda");
                 }else{
-                    System.out.println("Lucro");
+                    if(precoCusto > precoVenda) {
+                        System.out.println("Prejuízo");
+                    }else{
+                        System.out.println("Lucro");
+                    }
                 }
+
+                System.out.println(nomeProduto + ", preço de custo: = " + precoCusto 
+                + ", preço de venda = " + precoVenda);
             }
 
-            System.out.println(nomeProduto + ", preço de custo: = " + precoCusto 
-            + ", preço de venda = " + precoVenda);
+            System.out.println("A média do preço de custo é de: " + (totalCusto / i));
+            System.out.println("A média do preço de venda é de: " + (totalVenda / i));
         }
-
-        System.out.println("A média do preço de custo é de: " + (totalCusto / i));
-        System.out.println("A média do preço de venda é de: " + (totalVenda / i));
 
     }
 }
